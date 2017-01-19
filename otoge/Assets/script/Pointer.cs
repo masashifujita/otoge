ce24deb7598a6b51d4df0911fb5a6ece9c53291a
Ueki <kbc15b21@stu.kawahara.ac.jp>
2017-01-19 11:11:53 +0900

    エフェクト更新　タップ&ドラッグ

--- otoge/Assets/script/Pointer.cs
+++ otoge/Assets/script/Pointer.cs
@@ -4,24 +4,46 @@ using System.Collections;
 public class Pointer : MonoBehaviour
 {
     [SerializeField]
-    ParticleSystem PointerEffect;
+    GameObject Click_Effect;
     [SerializeField]
-    Camera Ecamera;
+    GameObject Drag_Effect;
+
+    private GameObject m_ClickEffect;
+    private GameObject m_DragEffect;
+    private ParticleSystem m_ClickParticle;
+    private ParticleSystem m_DragParticle;
+
     // Use this for initialization
     void Start()
     {
+        m_ClickEffect = (GameObject)Instantiate(Click_Effect);
+        m_DragEffect = (GameObject)Instantiate(Drag_Effect);
+
+        m_ClickParticle = m_ClickEffect.GetComponent<ParticleSystem>();
+        m_DragParticle = m_DragEffect.GetComponent<ParticleSystem>();
+        m_ClickParticle.Stop();
+        m_DragParticle.Stop();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 20f;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        m_DragEffect.transform.position = mousePosition;
+
         if (Input.GetMouseButtonDown(0))
         {
-            // マウスのワールド座標までパーティクルを移動し、パーティクルエフェクトを1つ生成する 
-            Vector3 pos = Ecamera.ScreenToWorldPoint(Input.mousePosition + Ecamera.transform.forward * 10);
-            PointerEffect.transform.position = pos;
-            PointerEffect.Emit(1);
+            m_ClickEffect.transform.position = mousePosition;
+            m_ClickParticle.Play();
+            m_DragParticle.Play();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            m_ClickParticle.Stop();
+            m_DragParticle.Stop();
         }
     }
 }
